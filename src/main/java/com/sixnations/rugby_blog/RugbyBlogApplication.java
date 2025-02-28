8a7a84acc8f717839862d07fb8d92ce8259a14a4
A00325334: Cathal Dawson Bolado <a00325334@student.tus.ie>
2025-02-28 23:04:45 +0000

    initial commit

---
+++ src/main/java/com/sixnations/rugby_blog/RugbyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.sixnations.rugby_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RugbyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RugbyBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sixnations/rugby_blog/RugbyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixnations.rugby_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RugbyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
